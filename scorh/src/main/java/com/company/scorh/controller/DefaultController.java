a44c77ab5521f50f8525a93fd9493c7c7930c8e5
pyq <alwayswannaflyx@gmail.com>
2019-12-28 14:30:36 +0800

    Update DefaultController.java

--- scorh/src/main/java/com/company/scorh/controller/DefaultController.java
+++ scorh/src/main/java/com/company/scorh/controller/DefaultController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DefaultController {
+    int a=1;
     @RequestMapping("/")
     public String index(){
         return "page/index.html";
